d377d65f768eb274ce41718389788f3774450b3a
Leandro Arturi <lnarturi@teco.com.ar>
2023-08-27 10:50:12 -0300

    Commit

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,10 @@ set GOOS=linux
 set GOARCH=amd64
 
 go build -tags lambda.norpc -o bootstrap main.go
-rm main.zip
-zip main.zip main
+# rm main.zip
+# zip main.zip main
 
-zip myFunction.zip bootstrap
+# zip myFunction.zip bootstrap
 
 
 # chmod +x deploy.sh
